260077715a56a3060f6513057b02465fa4c9ada7
chenyuewu <chenyue@linax1.cims.nyu.edu>
2015-03-07 15:41:26 -0500

    files added.

---
+++ array_ring.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include "util.h"
+
+int main(int argc, char **argv)
+{
+  if (argc != 2) {
+    fprintf(stderr, "Need parameter to specify how often the message is sent!");
+    return 1;
+  }
+
+  int N = atol(argv[1]);
+
+  if (N <= 0) {
+    fprintf(stderr, "Number of loops must be positive!");
+    return 2;
+  }
+
+  int rank, size;
+  MPI_Status status;
+
+  timestamp_type time0, time1;
+  get_timestamp(&time0);
+
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  int* message = (int*) malloc(2e6);
+
+  int i = 0;
+
+  if (rank == 0)
+    for (i = 0; i < 2e6/sizeof(int) ; i++)
+      message[i] = 0;
+
+  int destination = (rank + 1) % size;
+  int origin = (rank - 1) % size;
+  origin = origin < 0? origin + size : origin;
+  int tag = 99;
+
+  //printf("process %d has destination %d and origin %d.\n", rank, destination, origin);
+
+  for (i = 0; i < N; i++)
+  {
+    if (rank == 0) {
+      MPI_Send(message, 2e6/sizeof(int), MPI_INT, destination, tag, MPI_COMM_WORLD);
+      //printf("At loop %d, process %d send %d to process %d.\n", i, rank, message, destination);
+      MPI_Recv(message, 2e6/sizeof(int), MPI_INT, origin, tag, MPI_COMM_WORLD, &status);
+    }
+    else {
+      MPI_Recv(message, 2e6/sizeof(int), MPI_INT, origin, tag, MPI_COMM_WORLD, &status);
+      MPI_Send(message, 2e6/sizeof(int), MPI_INT, destination, tag, MPI_COMM_WORLD);
+      //printf("At loop %d, process %d send %d to process %d.\n", i, rank, message, destination);
+    }
+  }
+
+  if (rank == 0) {
+    //printf("After %d loops on %d processes, the integer grows to %d.\n", N, size, message);
+    get_timestamp(&time1);
+    double elapsed = timestamp_diff_in_seconds(time0,time1);
+    printf("Total communication: %d.\nTime elapsed: %f seconds.\nLatency: %f seconds.\n", size*N, elapsed, elapsed/size/N);
+    printf("Bandwidth %fGB/s\n",size*N*2e6/1e9/elapsed);
+  }
+  
+  MPI_Finalize();
+  return 0;
+
+}
